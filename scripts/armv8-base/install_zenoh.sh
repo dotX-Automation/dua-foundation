656a46627549c51d860136acc1c62b8b2ad2a2c9
Roberto Masocco <r.masocco@dotxautomation.com>
2026-03-13 19:23:01 +0100

    feat: armv8-base

---
+++ scripts/armv8-base/install_zenoh.sh
@@ -0,0 +1,122 @@
+#!/bin/sh
+
+# Zenoh libraries and tools installation script.
+#
+# Roberto Masocco <r.masocco@dotxautomation.com>
+#
+# February 11, 2025
+
+# Copyright 2025 dotX Automation s.r.l.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script assumes that:
+# - a Rust toolchain has been installed and its environment is configured;
+# - the desired Python virtual (or system) environment has been activated.
+
+set -e
+
+# Get base unit
+BASE_UNIT="${1-}"
+
+# Get the requested Zenoh version
+ZENOH_VERSION="${2-}"
+
+if [ "$BASE_UNIT" = "armv8-base" ]; then
+  echo "Installing Zenoh $ZENOH_VERSION for $BASE_UNIT ..."
+
+  ZENOH_ARCH="aarch64-unknown-linux-gnu"
+  ZENOH_PICO_ARCH="linux-arm64"
+
+  mkdir -p /etc/zenoh /opt/zenoh
+
+  # Install standalone Zenoh router, plugins, and sample configuration file
+  wget -O /etc/zenoh/DEFAULT_ZENOH_CONFIG.json5 https://raw.githubusercontent.com/eclipse-zenoh/zenoh/refs/tags/${ZENOH_VERSION}/DEFAULT_CONFIG.json5
+  wget -O /tmp/zenoh-standalone.zip https://github.com/eclipse-zenoh/zenoh/releases/download/${ZENOH_VERSION}/zenoh-${ZENOH_VERSION}-${ZENOH_ARCH}-standalone.zip
+  unzip /tmp/zenoh-standalone.zip -d /tmp
+  mv /tmp/zenohd /usr/local/bin/zenohd
+  mv /tmp/libzenoh_plugin_rest.so /usr/local/lib/
+  mv /tmp/libzenoh_plugin_storage_manager.so /usr/local/lib/
+  rm /tmp/zenoh-standalone.zip
+  chmod a+x /usr/local/bin/zenohd
+
+  # Install Zenoh DDS Bridge, plugin, and sample configuration file
+  wget -O /etc/zenoh/DEFAULT_DDS_CONFIG.json5 https://raw.githubusercontent.com/eclipse-zenoh/zenoh-plugin-dds/refs/tags/${ZENOH_VERSION}/DEFAULT_CONFIG.json5
+  wget -O /tmp/zenoh-plugin-dds-standalone.zip https://github.com/eclipse-zenoh/zenoh-plugin-dds/releases/download/${ZENOH_VERSION}/zenoh-plugin-dds-${ZENOH_VERSION}-${ZENOH_ARCH}-standalone.zip
+  unzip /tmp/zenoh-plugin-dds-standalone.zip -d /tmp
+  mv /tmp/zenoh-bridge-dds /usr/local/bin/zenoh-bridge-dds
+  mv /tmp/libzenoh_plugin_dds.so /usr/local/lib/
+  rm /tmp/zenoh-plugin-dds-standalone.zip
+  chmod a+x /usr/local/bin/zenoh-bridge-dds
+
+  # Install Zenoh ROS2DDS Bridge, plugin, and sample configuration file
+  wget -O /etc/zenoh/DEFAULT_ROS2DDS_CONFIG.json5 https://raw.githubusercontent.com/eclipse-zenoh/zenoh-plugin-ros2dds/refs/tags/${ZENOH_VERSION}/DEFAULT_CONFIG.json5
+  wget -O /tmp/zenoh-plugin-ros2dds-standalone.zip https://github.com/eclipse-zenoh/zenoh-plugin-ros2dds/releases/download/${ZENOH_VERSION}/zenoh-plugin-ros2dds-${ZENOH_VERSION}-${ZENOH_ARCH}-standalone.zip
+  unzip /tmp/zenoh-plugin-ros2dds-standalone.zip -d /tmp
+  mv /tmp/zenoh-bridge-ros2dds /usr/local/bin/zenoh-bridge-ros2dds
+  mv /tmp/libzenoh_plugin_ros2dds.so /usr/local/lib/
+  rm /tmp/zenoh-plugin-ros2dds-standalone.zip
+  chmod a+x /usr/local/bin/zenoh-bridge-ros2dds
+
+  chgrp -R internal /etc/zenoh
+  chmod -R g+rw /etc/zenoh
+  ldconfig
+
+  # Install Zenoh Python API
+  pip install eclipse-zenoh
+
+  # Build and install Zenoh C API, cleanup
+  git clone --depth 1 --single-branch \
+    --branch "${ZENOH_VERSION}" \
+    https://github.com/eclipse-zenoh/zenoh-c.git \
+    /opt/zenoh/zenoh-c
+  mkdir -p /opt/zenoh/zenoh-c/build
+  cd /opt/zenoh/zenoh-c/build
+  cmake \
+    -DBUILD_SHARED_LIBS=ON \
+    -DZENOHC_BUILD_WITH_SHARED_MEMORY=true \
+    ..
+  cmake --build . --config Release
+  cmake --build . --target install
+  cd /opt/zenoh
+  ldconfig
+
+  # Install Zenoh-Pico
+  wget -O /tmp/zenoh-pico.zip https://github.com/eclipse-zenoh/zenoh-pico/releases/download/${ZENOH_VERSION}/zenoh-pico-${ZENOH_VERSION}-${ZENOH_PICO_ARCH}-debian.zip
+  unzip /tmp/zenoh-pico.zip -d /tmp
+  dpkg -i /tmp/libzenohpico_*.deb /tmp/libzenohpico-dev_*.deb
+  rm /tmp/zenoh-pico.zip /tmp/libzenohpico_*.deb /tmp/libzenohpico-dev_*.deb
+  ldconfig
+
+  # Install Zenoh C++ API
+  git clone --depth 1 --single-branch \
+    --branch "${ZENOH_VERSION}" \
+    https://github.com/eclipse-zenoh/zenoh-cpp.git \
+    /opt/zenoh/zenoh-cpp
+  mkdir -p /opt/zenoh/zenoh-cpp/build
+  cd /opt/zenoh/zenoh-cpp/build
+  cmake \
+    -DZENOHCXX_ZENOHC=ON \
+    -DZENOHCXX_ZENOHPICO=ON \
+    ..
+  cmake --install .
+  cd /opt/zenoh
+  ldconfig
+
+  # Cleanup
+  cd /opt
+  rm -rf /opt/zenoh
+else
+  echo "ERROR: Invalid base unit '$BASE_UNIT'"
+  exit 1
+fi
---
+++ scripts/armv8-base/stage2.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+# Stage 2 script for armv8-base.
+#
+# Roberto Masocco <r.masocco@dotxautomation.com>
+#
+# March 13, 2026
+
+# Copyright 2026 dotX Automation s.r.l.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+# Source new Python virtual environment
+. /opt/dua-venv/bin/activate
+
+# Build and install Eigen
+cd /opt
+/opt/scripts/build_eigen.sh armv8-base 3.4.0
+chgrp internal /opt/scripts/build_eigen.sh
+chmod g+rwx /opt/scripts/build_eigen.sh
+cd /root
+
+# Build and install OpenCV
+cd /opt
+/opt/scripts/build_opencv.sh armv8-base 4.11.0
+chgrp internal /opt/scripts/build_opencv.sh
+chmod g+rwx /opt/scripts/build_opencv.sh
+cd /root
+
+# Install DDS implementations:
+# - Fast DDS
+# - Cyclone DDS
+# NOTES
+# - Only C/C++ libraries and support is installed.
+# - The versions are fixed to the closest ones supported by ROS 2 Jazzy (below).
+# - None of these installations goes to system paths, they remain confined
+#   within subdirectories of /opt/dds; see the scripts for details.
+#   In case you would like to use these libraries in your projects, you
+#   should configure CMake or whatever you use accordingly.
+#   This is to enable development with standalone DDS implementations while
+#   avoiding to create conflicts with the ROS 2 installation.
+cd /opt
+/opt/scripts/install_dds.sh armv8-base 2.14.5 0.10.5
+chgrp internal /opt/scripts/install_dds.sh
+chmod g+rwx /opt/scripts/install_dds.sh
+chgrp -R internal /opt/dds
+chmod -R g+rw /opt/dds
+cd /root
+
+# Install Zenoh
+cd /opt
+/opt/scripts/install_zenoh.sh armv8-base 1.7.1
+chgrp internal /opt/scripts/install_zenoh.sh
+chmod g+rwx /opt/scripts/install_zenoh.sh
+cd /root
