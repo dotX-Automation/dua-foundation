3b366dfeaa07870a94dc1b142652b7fecb3eb9fd
Roberto Masocco <r.masocco@dotxautomation.com>
2026-04-23 18:43:11 +0200

    feat: rclpy_message_converter, zenoh_json_bridge

--- scripts/armv8-base/install_ros2.sh
+++ scripts/armv8-base/install_ros2.sh
@@ -78,6 +78,7 @@ if [ "$BASE_UNIT" = "armv8-base" ]; then
     ros-$ROS_DISTRO-launch-testing-ros \
     ros-$ROS_DISTRO-perception \
     ros-$ROS_DISTRO-perception-pcl \
+    ros-$ROS_DISTRO-rclpy-message-converter \
     ros-$ROS_DISTRO-rmw-cyclonedds-cpp \
     ros-$ROS_DISTRO-rmw-fastrtps-cpp \
     ros-$ROS_DISTRO-robot-state-publisher \
